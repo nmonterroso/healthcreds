bb0d6088b03b392d2ecfe750c2bde27cc7f31c32
nmonterroso <nmonterroso@users.noreply.github.com>
2017-03-16 22:49:00 -0700

    remove localstorage stuff from store

--- src/store.js
+++ src/store.js
@@ -1,18 +1,12 @@
 import { createStore, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import throttle from 'lodash/throttle'
 
 import rootReducer from './reducers/rootReducer'
-// import { loginWithToken } from './actions'
-import { saveState, loadState } from './helpers'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-export const store = createStore(rootReducer, loadState(),
+export const store = createStore(rootReducer, {},
   composeEnhancers(
     applyMiddleware(thunk),
   ),
 )
-
-store.subscribe(throttle(() => saveState(store.getState()), 1000))
-// store.dispatch(loginWithToken())
