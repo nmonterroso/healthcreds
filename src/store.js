f44367a778a39a9f430302deec836edb188163a4
nmonterroso <nmonterroso@users.noreply.github.com>
2017-03-16 22:49:22 -0700

    whitespace

--- src/store.js
+++ src/store.js
@@ -1,6 +1,5 @@
 import { createStore, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-
 import rootReducer from './reducers/rootReducer'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
